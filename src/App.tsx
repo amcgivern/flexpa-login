3f28debd0b6bfd1cf3d87a4f80d14cdcc7d56d5b
Amanda McGivern <amanda.marie.mcgivern@gmail.com>
2022-03-13 16:13:34 -0400

    Load procedure data and show ids

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { FlexpaLogin } from "@flexpa/login"
-
+import { FlexpaLogin, useFHIRSearch, useFlexpaConfig } from "@flexpa/login"
+import { Procedure } from 'fhir/r4';
+ 
 function App() {
+
+
   return (
     <div className="App">
-      <FlexpaLogin sandbox />
+      <FlexpaLogin sandbox>
+          <ProcedureContainer/>
+        </FlexpaLogin> 
     </div>
   );
 }
 
+function ProcedureContainer () {
+  const { state } = useFlexpaConfig();
+  
+  if (!state.flexpaJwt) {
+    return (<div>ERROR</div>);
+  }
+
+  return (
+    <ProcedureList state={state}/>
+  )
+}
+
+function ProcedureList ({ state } : {state:any}) {// TODO - this state shouldn't be any type
+  const options = {
+    baseURL: 'https://api.sandbox.flexpa.com/', // state.gatewayUrl
+    authorization: state.flexpaJwt!,
+    query: { limit: '10',  offset: '5' }
+  }
+  const { data, error } = useFHIRSearch<Procedure>('Procedure', options);
+  
+  console.log("data", data);
+  return (
+    <div>
+    <div>PROCEDURES</div>
+    <div>
+      {data && data.map(procedure  => {
+        return <div key={procedure.id}> {procedure.id}</div>
+      })}
+    </div>
+  </div>)
+}
 export default App;
